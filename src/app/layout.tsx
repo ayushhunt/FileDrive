9df7fc4ecae0a058d256caf369b1ff985269d5c9
shadowmonarch12345 <138355840+shadowmonarch12345@users.noreply.github.com>
2024-05-24 14:12:21 +0530

    upload function done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
 import { Header } from "./header";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><ConvexClientProvider>
-      <Header />
-        {children}</ConvexClientProvider></body>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <Toaster />
+          <Header />
+          {children}
+        </ConvexClientProvider>
+      </body>
     </html>
   );
 }
